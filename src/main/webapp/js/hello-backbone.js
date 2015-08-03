08d823dbbb3214f70663f11a367e28f5830093cd
qzt05757 <qzt05757@nifty.ne.jp>
2015-08-04 06:36:10 +0900

    backbone 対応中

--- src/main/webapp/js/hello-backbone.js
+++ src/main/webapp/js/hello-backbone.js
@@ -3,33 +3,64 @@ function Person(first, last) {
   this.lastName =  last;
 };
 
-var Persons = Backbone.Model.extend({});
-persons = new Persons({
-	inputFirstName : "",
-	inputLastName : "",
-	persons: [],
-	addPerson: function(p) {
-		this.persons.push(p);
+var InputModel = Backbone.Model.extend({
+	firstName: "",
+	lastName: "",
+	create: function() {
+		var p = new Person(firstName, lastName);
+		personsModel.add(p);
+	}
+});
+var inputModel = new InputModel({});
+
+var InputView = Backbone.View.extend({ });
+var inputView = new InputView({
+	id: 'input-panel',
+	events: {
+		'change', 'updateModel'
+	},	
+    updateModel: function(){
+        var firstName = $('#firstName').val();
+        var lastName = $('#lastName').val();
+        this.model.set({firstName: firstName, lastName: lastName});
+    },
+	render: function() {
+		var firstName = this.model.get('firstName');
+		var lastName = this.model.get('lastName');
+		var fullName = firstName + ' ' + lastName;
+        this.$('#firstName').val(firstName);
+        this.$('#lastName').val(lastName);
+        this.$('#fullName').text(fullName);
 	},
 	create: function() {
-		var p = ner Person(this.inputFirstName, this.inputLastName);
+		
+	}	
+});
+
+var PersonsModel = Backbone.Collection.extend({
+	url: '/api/persons',
+	parse: function(jsons) {
+		var ret = [];
+		for(var i = 0; i < jsons.length; ++i) {
+			var json = jsons[i];
+			var p = new Person(json.firstName, json.lastName);
+			ret.push(p);
+		}
+		return ret;
+	}
+});
+var personsModel = new PersonsModel();
+
+var PersonsView = Backbone.View.extend({ });
+var personView = new PersonsView({
+	id: 'persons-panel',
+	render: function() {
+		
 	}
 });
 
-var PersonsView = Backbone.View.extend({
-	el: '#person',
-	events: {
-		'change': 'change'
-	},
-	initialize: function() {
-		this.listenTo(this.model, 'change', this.render);
-	},
-	change: function() {
-
-	},
-	render: function() {		
-
-	}
+personsModel.fetch().success(function(col, res, opts) {
+	col.trigger('onFetch');
 });
 
-var personsView = new PersonView({model: persons});
+
