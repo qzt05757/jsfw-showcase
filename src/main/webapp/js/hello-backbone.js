b9b9825011e046409760f472b902c40f236d3d21
Masashi Hayakawa <m-hayakawa@ce.jp.nec.com>
2015-08-04 14:20:53 +0900

    とりあえず、Backbone が動作する

--- src/main/webapp/js/hello-backbone.js
+++ src/main/webapp/js/hello-backbone.js
@@ -1,44 +1,86 @@
 function Person(first, last) {
-  this.firstName = first;
-  this.lastName =  last;
+    this.firstName = first;
+    this.lastName =  last;
 };
 
-var InputModel = Backbone.Model.extend({
-	firstName: "",
-	lastName: "",
-	create: function() {
-		var p = new Person(firstName, lastName);
-		personsModel.add(p);
-	}
-});
-var inputModel = new InputModel({});
+var InputModel = Backbone.Model.extend({});
 
-var InputView = Backbone.View.extend({ });
+var inputModel = new InputModel({
+	'firstName': '',
+	'lastName': ''
+});
+
+var InputView = Backbone.View.extend({
+    initialize: function() {
+        this.listenTo(this.model, 'change', this.render);
+        this.render();
+    },
+    events: {
+        'change': 'updateModel',
+        'click button#create': 'createPerson'
+    },
+    updateModel: function() {
+        var firstName = this.$el.find('#firstName').val();
+        var lastName = this.$el.find('#lastName').val();
+        this.model.set({'firstName': firstName, 'lastName': lastName});
+    },
+    render: function() {
+        var firstName = this.model.get('firstName');
+        var lastName = this.model.get('lastName');
+        var fullName = firstName + ' ' + lastName;
+        this.$el.find('#firstName').val(firstName);
+        this.$el.find('#lastName').val(lastName);
+        this.$el.find('#fullName').text(fullName);
+    },
+    createPerson: function() {
+        this.updateModel();
+        var p = new Person(this.model.get('firstName'), this.model.get('lastName'));
+        personsModel.add(p);        
+    },
+});
 var inputView = new InputView({
-	id: 'input-panel'
+    el   : $('#input-panel'),
+    model: inputModel
 });
 
 var PersonsModel = Backbone.Collection.extend({
-	url: '/api/persons',
+    url: 'api/persons',
 	parse: function(jsons) {
-		var ret = [];
-		for(var i = 0; i < jsons.length; ++i) {
-			var json = jsons[i];
-			var p = new Person(json.firstName, json.lastName);
-			ret.push(p);
-		}
+        var ret = [];
+        for(var i = 0; i < jsons.length; ++i) {
+		    var json = jsons[i];
+		    var p = new Person(json.firstName, json.lastName);
+		    ret.push(p);
+        }
 		return ret;
-	}
+    }
 });
 var personsModel = new PersonsModel();
 
-var PersonsView = Backbone.View.extend({ });
+var PersonsView = Backbone.View.extend({
+    initialize: function() {
+        this.listenTo(this.model, 'add', this.render);
+        this.renderAll();
+    },
+    renderAll: function() {
+    	this.model.each(function(x) {
+    		this.render()
+    	});
+    	for(var i = 0; i < this.model.length; ++i) {
+    		var m = model.at(i);
+    		this.render(m);    		
+    	}
+    },
+    render: function(add) {
+        var $div = this.$el;
+        var $tbody = $div.find('table tbody');
+        var html = "<tr><td>" + add.get('firstName') + "</td><td>" + add.get('lastName') + "</td></tr>";
+        $tbody.append(html);
+    }
+});
 var personView = new PersonsView({
-	id: 'persons-panel'	
+    el: $('#persons-panel'),
+    model: personsModel
 });
 
-personsModel.fetch().success(function(col, res, opts) {
-	col.trigger('onFetch');
-});
-
-
+personsModel.fetch().success(function(col, res, opts) { });
