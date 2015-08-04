6052fd973f5a89f6d4990f432ecd638213aa7bb3
qzt05757 <qzt05757@nifty.ne.jp>
2015-08-04 21:35:35 +0900

    react 版がとりあえず動作

--- src/main/webapp/js/hello-react.js
+++ src/main/webapp/js/hello-react.js
@@ -4,40 +4,85 @@ function Person(first, last) {
 };
 
 var InputArea = React.createClass({
+	getInitialState: function() {
+		return {
+			firstName: '',
+			lastName: ''
+		};
+	},
+	
+	onClick: function() {
+		this.props.addPerson(this.state.firstName, this.state.lastName);
+	},
+	
+	changeFirstName: function(e) {
+		this.setState({firstName: e.target.value});
+	},
+	
+	changeLastName: function(e) {
+		this.setState({lastName: e.target.value});
+	},
+	
 	render: function() {
+		var fullName = this.state.firstName + ' ' + this.state.lastName;
 		return <div id="input-panel" className="panel-body">
-                 <p>FirstName: <input id="firstName" className="form-control"/></p>
-                 <p>LastName: <input id="lastName" className="form-control"/></p>
-                 <p>FullName: <span id="fullName"></span></p>
-                 <button id="create" className="btn">create</button>
+                 <p>FirstName: <input value={this.state.firstName} className="form-control" onChange={this.changeFirstName}/></p>
+                 <p>LastName: <input value={this.state.lastName} className="form-control" onChange={this.changeLastName}/></p>
+                 <p>FullName: {fullName}</p>
+                 <button className="btn" onClick={this.onClick}>create</button>
                </div>;
 	}
 });
 
 var PersonsArea = React.createClass({
+	
+	getInitialState: function() {
+		return {
+			persons: []
+		};
+	},
+	
+	componentDidMount: function() {
+		var self = this;
+		$.getJSON('api/persons', function(jsons) {
+			for(var i = 0; i < jsons.length; ++i) {
+				var json = jsons[i];
+				self.addPerson(json.firstName, json.lastName);				
+			}
+		});
+	},
+	
+	addPerson: function(firstName, lastName) {
+		var p = new Person(firstName, lastName);
+		var ps = this.state.persons;
+		ps.push(p);
+		this.setState({persons: ps});		
+	},
+	
 	render: function() {
-		return <div className="persons-area">
-	             <table className="table table-striped table-bordered">
-	               <thead>
-	                 <tr><th>FirstName</th><th>LastName</th></tr>
-	               </thead>
-	               <tbody>
-	               </tbody>
-	             </table>
+		var ps = this.state.persons;
+		
+		var tbody = ps.map(function(p, idx) {
+			return <tr key={idx}><td>{p.firstName}</td><td>{p.lastName}</td></tr>;			
+		});
+
+		return <div>
+		         <InputArea addPerson={this.addPerson}/>
+                 <div id="persons-panel">
+  		           <table className="table table-striped table-bordered">
+	                 <thead>
+	                   <tr><th>FirstName</th><th>LastName</th></tr>
+	                 </thead>
+	                 <tbody>
+	                 {tbody}
+	                 </tbody>
+	               </table>
+	             </div>
 	           </div>;
 	}	
 });
 
-var HelloApplication = React.createClass({
-    render: function() {
-      return <div>
-               <InputArea/>
-               <PersonsArea/>
-             </div>;
-    }
-});
-
 React.render(
-    <HelloApplication/>,
-    $('#container').get(0)
+    <PersonsArea/>,
+    $('#contentArea').get(0)
 );
