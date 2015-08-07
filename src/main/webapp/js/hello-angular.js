fdf4fedce951f9a420c7a6277db218383f78d0d2
Masashi Hayakawa <m-hayakawa@ce.jp.nec.com>
2015-08-08 01:52:52 +0900

    angular 修正中

--- src/main/webapp/js/hello-angular.js
+++ src/main/webapp/js/hello-angular.js
@@ -1,1 +1,24 @@
-angular.module('App', []);
+var appModule = angular.module('App', []);
+appModule.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+    function Person(first, last) {
+        this.firstName = first;
+        this.lastName =  last;
+    };
+
+    $scope.inputFirstName = '';
+    $scope.inputLastName = '';
+
+    $scope.getFullName = function() {
+        return $scope.inputFirstName + ' ' + $scope.getLastName;
+    };
+
+    $scope.persons = [];
+
+    $scope.addPerson = function() {
+        var p = new Person($scope.firstName, $scope.lastName);
+        $scope.persons.push(p);
+    };
+}]);
+
+
+     
