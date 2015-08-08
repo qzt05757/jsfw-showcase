8db1f810d6c72a524759d1c072e8dfe48bf5bba6
qzt05757 <qzt05757@nifty.ne.jp>
2015-08-08 23:30:47 +0900

    reactive 版がとりあえず動く

--- src/main/webapp/js/hello-ractive.js
+++ src/main/webapp/js/hello-ractive.js
@@ -1,5 +1,39 @@
-var ractive = new Ractive({
+var model = new Ractive({
     el: '#container',
-    template: '#template',
-    data: { name: 'world' }
+    template: '#hello',
+    data: { 
+    	inputFirstName: '',
+    	inputLastName: '',
+    	inputFullName: '',
+    	persons: []
+    },
+    
+    addPerson: function(firstName, lastName) {
+    	var p = {firstName: firstName, lastName: lastName};
+    	this.get('persons').push(p);
+    },
+
+    onrender: function() {
+    	var self = this;
+    	
+    	this.observe('inputFirstName inputLastName', function() {
+    		var f = this.get('inputFirstName');
+    		var l = this.get('inputLastName');
+    		this.set('inputFullName', f + ' ' + l);
+    	});
+    	
+    	this.on('create', function(e) {
+    		var f = this.get('inputFirstName');
+    		var l = this.get('inputLastName');
+    		self.addPerson(f, l);    		
+    	});
+    	
+		$.getJSON("api/persons", function(ps) {
+			for(var i = 0; i<ps.length; ++i) {
+				var p = ps[i];
+				self.addPerson(p.firstName, p.lastName);
+			}
+		});
+    }
 });
+
