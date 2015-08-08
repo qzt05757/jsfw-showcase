1485f4e5a332ece46f187e51b7440ca752bc6fe3
qzt05757 <qzt05757@nifty.ne.jp>
2015-08-08 22:49:42 +0900

    とりあえず amber.js が動く状態

--- src/main/webapp/js/hello-ember.js
+++ src/main/webapp/js/hello-ember.js
@@ -1,6 +1,44 @@
 window.App = Ember.Application.create();
-App.ApplicationRoute = Ember.Route.extend({
+
+App.Router.map(function() {
+	this.route('hello', {path: '/'});
+});
+
+App.HelloRoute = Ember.Route.extend({
     model: function() {
-        return { name: 'Ember.js' };
+        return {
+        	persons: []
+        };
     }
+});
+
+App.HelloController = Ember.Controller.extend({
+	inputFirstName: '',
+	inputLastName: '',
+	inputFullName: function() {
+		return this.get('inputFirstName') + ' ' + this.get('inputLastName');
+	}.property('inputFirstName', 'inputLastName'),
+
+	init: function() {
+		var self = this;
+		$.getJSON("api/persons", function(ps) {
+			for(var i = 0; i<ps.length; ++i) {
+				var p = ps[i];
+				self.addPerson(p.firstName, p.lastName);
+			}
+		});
+	},
+
+	addPerson: function(firstName, lastName) {
+		var p = { firstName: firstName, lastName: lastName};
+		this.model.persons.pushObject(p);		
+	},
+	
+	actions: {
+		create: function() {
+			var firstName = this.get('inputFirstName');
+			var lastName = this.get('inputLastName');
+			this.addPerson(firstName, lastName);
+		}
+	},
 });
